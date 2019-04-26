77c1f2250edd8e28aff5e568261408b835493594
Jonathan Riordan <jriordan22849@gmail.com>
2019-04-26 15:30:46 +0100

    update to the png width

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const ChartjsNode = require('chartjs-node');
 var CHS = 'chs'
 var CHD = "chd";
 var CHS = "chs";
-var CHT = "cht";
 var CHL = "chl";
 var CHDL = "chdl";
 var CHCO = "chco";
@@ -16,17 +15,18 @@ const PORT = process.env.PORT || 3000;
 
 // seperators
 var BAR = "|";
-var COLON = ":"
-var COMMA = ","
+var COLON = ":";
+var SEMI_COLON = ";";
+var COMMA = ",";
+var X = "x";
 
 app.get(PATH, function(req, res){
     var paramatersByKey = getURLParameters(req);
-    console.log(paramatersByKey);
 
     var chartData = paramatersByKey.get(CHD).split(COLON);
     var data = chartData[1].split(COMMA);
 
-    var chartWidthHeight = paramatersByKey.get(CHS).split("x");
+    var chartWidthHeight = paramatersByKey.get(CHS).split(X);
 
     var chartLabels = paramatersByKey.get(CHL).split(BAR);
     var chartDataLabels =  paramatersByKey.get(CHDL).split(BAR);
@@ -35,34 +35,10 @@ app.get(PATH, function(req, res){
     var chartColours = paramatersByKey.get(CHCO).split(BAR);
     hexColours = addHashTags(chartColours)
     
-    var chartJsOptions = {
-      type: 'pie',
-      data: {
-        labels: labels,
-        datasets: [{
-          data: data,
-          backgroundColor: hexColours,
-          borderWidth: 0
-        }]
-      },
-      options: {
-         title: {
-          display: false,
-         },
-         chartArea: {
-            backgroundColor: '#ffffff'
-         },
-         responsive: false,
-         legend: {
-            position: 'right'
-         }
-      }
-    };
-    
+    var chartJsOptions = createChartOptions(labels, data, hexColours);
+    let randomCharacters = random();
     var height = chartWidthHeight[0];
     var width = chartWidthHeight[1];
-
-    let uniqueNumber = Math.random().toString(36).substring(7);
 
     var chartNode = new ChartjsNode(height, width);
     return chartNode.drawChart(chartJsOptions)
@@ -74,26 +50,47 @@ app.get(PATH, function(req, res){
         return chartNode.getImageStream('image/png');
     })
     .then(streamResult => {
-        // using the length property you can do things like
-        // directly upload the image to s3 by using the
-        // stream and length properties
-        streamResult.stream // => Stream object
-        streamResult.length // => Integer length of stream
-        // write to a file
-        return chartNode.writeImageToFile('image/png', './'+ uniqueNumber + '.png');
+        return chartNode.writeImageToFile('image/png', './'+ randomCharacters + '.png');
     })
     .then(() => {
-        res.sendFile('./'+ uniqueNumber + '.png', { root: __dirname });
+        res.sendFile('./'+ randomCharacters + '.png', { root: __dirname });
     });
   }
 );
+
+function createChartOptions(labels, data, hexColours) {
+  var chartJsOptions = {
+    type: 'pie',
+    data: {
+      labels: labels,
+      datasets: [{
+        data: data,
+        backgroundColor: hexColours,
+        borderWidth: 0
+      }]
+    },
+    options: {
+       title: {
+        display: false,
+       },
+       chartArea: {
+          backgroundColor: '#ffffff'
+       },
+       responsive: false,
+       legend: {
+          position: 'right'
+       }
+    }
+  };
+  return chartJsOptions;
+}
 
 function getURLParameters(req) {
   var paramatersByKey = new Map();
 
   for (const key in req.query) {
     var paramter = req.query[key];
-    var k = key.split(";");
+    var k = key.split(SEMI_COLON);
     if (k.length > 1) {
       paramatersByKey.set(k[1], paramter);
     }
@@ -115,10 +112,14 @@ function createLabels(statues, timeLabels) {
   var labels = [];
   var labelsSize = statues.length;
   for (i = 0; i < labelsSize; i++) {
-    var newLabel = statues[i] + "-" + timeLabels[i];
+    var newLabel = statues[i]; //+ "-" + timeLabels[i];
     labels.push(newLabel);
   }
   return labels;
+}
+
+function random() {
+  return Math.random().toString(36).substring(7);
 }
 
 app.listen(PORT); 
