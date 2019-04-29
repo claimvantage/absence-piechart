2738c3d34484d0bb89fa7a09afd60d20c7d72c3e
Jonathan Riordan <jriordan22849@gmail.com>
2019-04-29 11:39:54 +0100

    update inage width

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.get(PATH, function(req, res) {
 function response(res, options, chartWidthHeight) {
   let width = chartWidthHeight[0];
   let height = chartWidthHeight[1];
-  let chartNode = new ChartjsNode(500, height);
+  let chartNode = new ChartjsNode(500, 90);
 
   return chartNode.drawChart(options)
     .then(() => {
