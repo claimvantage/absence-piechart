6f8ec79ff63604b48bb7854fd51a38e2443ef05d
Jonathan Riordan <jriordan22849@gmail.com>
2019-05-02 16:35:23 +0100

    Update for chart padding

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ function start() {
       options: {
         layout: {
           padding: {
-              left: -50,
+              left: -250,
               top: 0,
               bottom: 0
           }
